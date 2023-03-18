8cd8175b4b9f4fa8e38eb44d4cf038c65c08368c
Mahmoud Alshahin <mahmoudshaheen198811@gmail.com>
2023-03-19 02:31:37 +0300

    Done with love

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(arr,str){
+const  newArr = []    
+ for(let i = 0  ;i < arr.length  ; i++){
+    newArr[i] =`Thank you, ${arr[i]}, for the wonderful ${str} gift!`
+   
+   
+ }
+ return newArr
+}
+function countDown(num){
+    let i = 0;
+    let downNumber = num ;
+    while(i < num){
+        console.log(downNumber)
+        downNumber -= 1
+        i += 1
+        
+    }
+    console.log(0)
+}
